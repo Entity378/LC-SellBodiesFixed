eb76d830b70a293f75689964ebd91c78d767af85
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-04-06 23:30:10 +0200

    Update 1.7.1

---
+++ SellBodies/Patches/EnemyAIPosPatcher.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Collections;
+
+namespace CleaningCompany.Patches
+{
+    [HarmonyPatch(typeof(EnemyAI))]
+    internal class EnemyAIPosPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("KillEnemyOnOwnerClient")]
+        static void MoveBody(EnemyAI __instance)
+        {
+            string name = __instance.enemyType.enemyName;
+            if (Plugin.instance.BodySpawns.ContainsKey(name))
+            {
+                __instance.StartCoroutine(MoveOldBody(__instance));
+            }
+            else if (Plugin.cfg.MODDEDENEMY && !Plugin.instance.VanillaBody.Contains(name))
+            {
+                __instance.StartCoroutine(MoveOldBody(__instance));
+            }
+        }
+
+        static IEnumerator MoveOldBody(EnemyAI __instance)
+        {
+            yield return new WaitForSeconds(4);
+
+            Vector3 OriginalBodyPos = new Vector3(-10000, -10000, -10000);
+            __instance.transform.position = OriginalBodyPos;
+            __instance.SyncPositionToClients();
+        }
+    }
+}
