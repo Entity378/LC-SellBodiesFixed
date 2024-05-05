6c150bc283056b18fc897da25a877586e4c39349
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-05-06 00:09:13 +0200

    1.8.5

--- SellBodies/Monos/BodySyncer.cs
+++ SellBodies/Monos/BodySyncer.cs
@@ -1,3 +1,5 @@
+using CleaningCompany.Patches;
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -11,6 +13,7 @@ namespace CleaningCompany.Monos
 
             if (IsHost || IsServer)
             {
+                Quaternion bodyRotation = KillEnemyServerRpcPatcher.publicBodyRotation;
                 PhysicsProp prop = GetComponent<PhysicsProp>();
                 int priceBase = Random.Range(prop.itemProperties.minValue, prop.itemProperties.maxValue);
                 int totalPowerCount;
@@ -20,20 +23,19 @@ namespace CleaningCompany.Monos
                 if (Plugin.cfg.DISABLE_MULTIPLIER == false)
                 {
                     totalPowerCount = StartOfRound.Instance.currentLevel.maxEnemyPowerCount + StartOfRound.Instance.currentLevel.maxOutsideEnemyPowerCount;
-                    priceMuliplier = ((((float)totalPowerCount - Plugin.cfg.MULTIPLIER_POWER_COUNT_SUBTRACTION) / 100f) * Plugin.cfg.MULTIPLIER_VALUE);
-                    price = (int)(priceBase + (int)(priceBase * priceMuliplier));
+                    priceMuliplier = (totalPowerCount - Plugin.cfg.MULTIPLIER_POWER_COUNT_SUBTRACTION) / 100f * Plugin.cfg.MULTIPLIER_VALUE;
+                    price = priceBase + (int)(priceBase * priceMuliplier);
                 }
                 else
                 {
                     price = priceBase;
                 }
-
-                SyncDetailsClientRpc(price, new NetworkBehaviourReference(prop));
+                SyncDetailsClientRpc(price, bodyRotation, new NetworkBehaviourReference(prop));
             }
         }
 
         [ClientRpc]
-        void SyncDetailsClientRpc(int price, NetworkBehaviourReference netRef)
+        void SyncDetailsClientRpc(int price, Quaternion rot, NetworkBehaviourReference netRef)
         {
             netRef.TryGet(out PhysicsProp prop);
             if (prop != null)
@@ -42,8 +44,19 @@ namespace CleaningCompany.Monos
                 prop.itemProperties.creditsWorth = price;
                 prop.GetComponentInChildren<ScanNodeProperties>().subText = $"Value: ${price}";
                 Debug.Log("Successfully synced body values");
+                StartCoroutine(RotateBodyClient(prop, rot));
             }
             else Debug.LogError("Failed to resolve network reference!");
+        }
+
+        static IEnumerator RotateBodyClient(PhysicsProp prop, Quaternion rot)
+        {
+            yield return new WaitForSeconds(0);
+            while (!prop.hasHitGround)
+            {
+                Debug.Log("Waiting for the body to hit the ground");
+            }
+            prop.GetComponent<Transform>().transform.SetPositionAndRotation(prop.transform.position, rot);
         }
     }
 }
