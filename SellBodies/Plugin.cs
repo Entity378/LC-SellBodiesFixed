4d4277cd8894f8639ea3423577205a6e710bee61
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-07-08 20:42:42 +0200

    Update 1.9.1

--- SellBodies/Plugin.cs
+++ SellBodies/Plugin.cs
@@ -17,7 +17,7 @@ namespace CleaningCompany
         readonly Harmony harmony = new Harmony(GUID);
         const string GUID = "Entity378.sellbodies";
         const string NAME = "Sell Bodies";
-        const string VERSION = "1.9.0";
+        const string VERSION = "1.9.1";
 
         static string root = "Assets/CleaningAssets/";
 
